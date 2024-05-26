4c44350483936407e5aac88510c5ebbd7fb8211c
ziauddin-0987 <ziauddink100@gmail.com>
2024-05-27 01:25:37 +0500

    this code change  html flime

---
+++ script.js
@@ -0,0 +1,50 @@
+// const button_on = document.getElementById("btn");
+// // this line is button
+
+// const un_oder_list = document.getElementById("oder");
+// const heading = document.querySelector("h1");
+// // this line is a un oder list
+
+// const listt = document.querySelectorAll("li");
+
+// const nav_bar = document.getElementById("bar");
+// const icon1 = document.querySelector(".fa-bars");
+// // const heading = document.querySelector("heading");
+
+// let show = 1;
+
+// // this line is  vav bar
+// icon1.addEventListener("click", () => {
+//   if (show == 1) {
+//     console.log("ON ");
+//     show = 0;
+//     nav_bar.style.width = "100%";
+//     nav_bar.style.height = "100px";
+//     un_oder_list.style.fontSize = "20px";
+//     un_oder_list.style.display = "block";
+//     nav_bar.style.display = "block";
+//     un_oder_list.style.color = "white";
+//     heading.style.display = "none";
+//   } else if (show == 0) {
+//     console.log("OFF");
+//     show = 1;
+//     // list1.style.display = "block";
+//     un_oder_list.style.display = "block";
+//     nav_bar.style.height = "4rem";
+//     // icon1.style.display = "flex";
+//     heading.style.display = "block";
+
+//     // icon1.style.top="1 "
+//   }
+// });
+
+var namedSum = function sum(a, b) {
+  // named
+  return a + b;
+};
+var anonSum = function (a, b) {
+  // anonymous
+  return a + b;
+};
+console.log(anonSum(5, 3));
+anonSum(1, 3);
